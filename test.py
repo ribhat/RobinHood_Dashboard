16aef5d0c726aba2a7b7db0cedae254101764d8c
ribhat <bhat.rish@gmail.com>
2023-10-06 16:54:42 -0700

    Added DataTable of Dividends by Stock YTD to the Dashboard

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import pandas as pd
 import datetime
 
 # Dash Imports
-from dash import Dash, dcc, Output, Input, html  # pip install dash
+from dash import Dash, dcc, Output, Input, html, dash_table  # pip install dash
 import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
 import plotly.express as px
 
@@ -37,8 +37,11 @@ def ViewHoldings():
     """Basic function to view breakdown of all user holdings
 
         Returns: None"""
+    list_of_user_tickers = []
     for key, value in my_stocks.items():
         print(key, value)
+        list_of_user_tickers.append(key)
+    return list_of_user_tickers
 
 
 def Quote(ticker):
@@ -51,7 +54,7 @@ def Quote(ticker):
 
 
 def CreatePieChart():
-    """This function creates a pie chart of the users current holdings
+    """This function creates a pie chart of the users current holdings within Python (not on dashboard)
 
         Returns: None"""
     slices = []
@@ -70,7 +73,7 @@ def CreatePieChart():
     plt.show()
 
 
-def DividendHistory(year=default_year):
+def TotalDividendsYTD(year=default_year):
     """This function will be used to visualize the dividend payouts by each month of the current year in a line graph.
         the x axis will be months and the y axis will be total dividends collected that month.
 
@@ -81,7 +84,7 @@ def DividendHistory(year=default_year):
     dividends_collected = []
 
     for month in months:
-        dividends_collected.append(TotalDivendsForMonth(month, year))
+        dividends_collected.append(TotalDivendsPerMonthYTD(month, year))
 
     # plt.plot(months, dividends_collected, color='#5a7d9a', linestyle='--', marker='o')  # -- means a dashed line
     # plt.title('Dividends Collected by Month')
@@ -95,7 +98,7 @@ def DividendHistory(year=default_year):
     return float("%.2f" % sum)
 
 
-def TotalDivendsForMonth(month, year=default_year):
+def TotalDivendsPerMonthYTD(month, year=default_year):
     """Helper function to return total dividends acquired in a particular month
         Inputs:
         month is str of the month name or str of month number
@@ -112,13 +115,17 @@ def TotalDivendsForMonth(month, year=default_year):
             sum += float(dictionary['amount'])
     return float("%.2f" % sum)
 
+def TotalDividendsPerStockYTD(ticker, year = default_year):
+    sum = 0
+
+
 
 # CreatePieChart()
 # Quote()
 # ViewHoldings()
 # print(TotalDivendsForMonth('June', 2023))
 # DividendHistory(2023)
-print(dividend_data[0])
+# print(dividend_data[0])
 response = requests.get(dividend_data[0]['instrument'])
 #print(response.json())
 
@@ -139,17 +146,28 @@ for i in range(len(dividend_data)):
     tickers.append(response.json()['symbol'])
 dividend_df['Ticker'] = tickers
 
+
 print(dividend_df.head())
+
+## filter dividend_df for results only from 2023 and group them by ticker while summing the amount column
+dividend_df['amount'] = dividend_df['amount'].astype(float).round(2)
+dividend_this_year_df = dividend_df[dividend_df['payable_date'].str.startswith('2023')]
+print(dividend_this_year_df)
+
+amounts_this_year_df = dividend_this_year_df.groupby('Ticker')['amount'].sum().reset_index() # the.reset_index helps us not keep the ticker as our primary key of the df
+print("Here is amounts_this_year df: \n")
+print(amounts_this_year_df)
+print("done")
 
 
 ## filter dividend_df for results only from this year
-print(dividend_df['payable_date'], print(str(default_year)))
-print(type(dividend_df['payable_date']), type(str(default_year)))
-
-dividend_this_year_df = dividend_df.loc[dividend_df['payable_date'] == str(default_year)]
-print("\nThis is the dividend_this_year_df")
-print(dividend_this_year_df.head())
-print(dividend_this_year_df.shape[0])
+# print(dividend_df['payable_date'], print(str(default_year)))
+# print(type(dividend_df['payable_date']), type(str(default_year)))
+#
+# dividend_this_year_df = dividend_df.loc[dividend_df['payable_date'] == str(default_year)]
+# print("\nThis is the dividend_this_year_df")
+# print(dividend_this_year_df.head())
+# print(dividend_this_year_df.shape[0])
 
 total_holdings_df = pd.DataFrame(my_stocks.values(), index=my_stocks.keys())
 #print(total_holdings_df[0:5])
@@ -160,7 +178,8 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
 
 ## Build Components
 
-header = dcc.Markdown(children='# Portfolio Dashboard')
+header = dcc.Markdown(children='# Dividend Dashboard')
+
 mygraph = dcc.Graph(figure={})
 
 slices = []
@@ -169,6 +188,34 @@ for ticker, details in my_stocks.items():
     labels.append(ticker)
     slices.append(float(details['equity']))
 mypie = dcc.Graph(figure=px.pie(names=labels, values=slices, hover_name=labels))
+
+
+amounts_this_year_df = amounts_this_year_df.sort_values(by='amount', ascending=False) #sort biggest to smallest
+# Define columns list for DataTable
+columns_list = []
+for col_name in amounts_this_year_df.columns:
+    if col_name == "amount":
+        column_dict = {
+            "name": "Dividends This Year",
+            "id": col_name,
+            "type": "numeric",
+            "format": {"specifier": ".2f"}
+        }
+    else:
+        column_dict = {
+            "name": col_name,
+            "id": col_name
+        }
+    columns_list.append(column_dict)
+# Define DataTable
+DividendTableYTD = dash_table.DataTable(
+    id='table',
+    columns=columns_list,
+    data=amounts_this_year_df.to_dict('records'),
+    style_table={'overflowY': 'auto'},
+    style_cell={'color': 'blue'}
+)
+
 dropdown = dcc.Dropdown(options=['Bar Plot', 'Scatter Plot', 'Line Graph'],
                         value='Bar Plot',  # initial value displayed when page first loads
                         clearable=False)
@@ -179,8 +226,8 @@ current_dt = str(datetime.datetime.now())
 curr_month = current_dt[5:7]
 curr_year = current_dt[0:4]
 dividends_this_month = dcc.Markdown(
-    children="Dividends this month: $" + str(TotalDivendsForMonth(str(curr_month), curr_year)))
-dividends_this_year = dcc.Markdown(children="Dividends so far this year: $" + str(DividendHistory(curr_year)))
+    children="Dividends this month: $" + str(TotalDivendsPerMonthYTD(str(curr_month), curr_year)))
+dividends_this_year = dcc.Markdown(children="Dividends so far this year: $" + str(TotalDividendsYTD(curr_year)))
 
 # Design App Layout
 
@@ -189,16 +236,16 @@ app.layout = dbc.Container([
         dbc.Col([header], width=6)
     ], justify='center'),
     dbc.Row([
-        dbc.Col([mypie], width=6), dbc.Col([mygraph], width=6)
+        dbc.Col([mypie], width=4), dbc.Col([mygraph], width=4), dbc.Col([DividendTableYTD], width=4)
     ]),
     dbc.Row([
-        dbc.Col(), dbc.Col([dropdown], width=6)
+        dbc.Col(), dbc.Col([dropdown], width=4), dbc.Col()
     ], justify='right'),
     dbc.Row([
-        dbc.Col([portfolio_value], width=6), dbc.Col([dividends_this_month], width=6)
+        dbc.Col([portfolio_value], width=4), dbc.Col(), dbc.Col([dividends_this_month], width=4)
     ]),
     dbc.Row([
-        dbc.Col(), dbc.Col([dividends_this_year], width=6)
+        dbc.Col(), dbc.Col(), dbc.Col([dividends_this_year], width=4)
 
 
     #Include a scrollable table that lists the dividends recieved this year
@@ -215,7 +262,7 @@ def update_graph(user_input):  # function arguments come from the component prop
     dividends_collected = []
 
     for month in months:
-        dividends_collected.append(TotalDivendsForMonth(month, 2023)) #create an array that represents dividends collected each month
+        dividends_collected.append(TotalDivendsPerMonthYTD(month, 2023)) #create an array that represents dividends collected each month
 
     if user_input == 'Bar Plot':
         fig = px.bar(data_frame=dividend_df, x=months, y=dividends_collected, title='Dividend Breakdown by Month')
